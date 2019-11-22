3ec050a9f44117924e37c12adad96d7feab24a58
Dannywuk <47495569+Dannywuk@users.noreply.github.com>
2019-11-22 09:27:45 +0800

    Update main.js

--- Secret Santa/main.js
+++ Secret Santa/main.js
@@ -3,7 +3,7 @@ window.onload = function(){
 	setTimeout(drawList, 3000);
 };
 
-let give = [];
+let give = ['Mr.A','Mr.Y'];
 let secretSanta = 'Mr.X';
 let people = document.getElementById('people');
 let load = document.getElementById('loading');
